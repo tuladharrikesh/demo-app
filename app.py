3329fd8316c9a0068953199204dcc20cd892d574
Rikesh Tuladhar <rituladh@amazon.com>
2023-02-24 13:07:34 -0600

    modified app.py, base.html and list.html

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import os
 import boto3 
+import re
 import json
 import psycopg2
 from flask import Flask, render_template
 from flask import Flask, render_template, request, url_for, redirect
 from botocore.exceptions import ClientError
-
-#pip install Flask psycopg2
-
 
 
 app = Flask(__name__)
@@ -15,45 +13,13 @@ app = Flask(__name__)
 
 def get_db_connection():
 
-    """secret_name = "rds!db-96874cdd-abcf-471d-bb2b-71f0ead3c827"
-    region_name = "us-east-1"
-
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        # For a list of exceptions thrown, see
-        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        raise e
-
-    # Decrypts secret using the associated KMS key.
-    secret = get_secret_value_response['SecretString'] """
-
-
-    
-    # secret_name = os.environ.get("SECRET_NAME")
-    secret_name = "{}/demo-app/postgresql".format(os.environ.get('COPILOT_ENVIRONMENT_NAME')) # test secret name
-    # secret_name = "demo-app/prod/postgreSql" # prod secret name
-
-
-
-
-    #secret_name = "rds!db-96874cdd-abcf-471d-bb2b-71f0ead3c827"
+    # Gets the preset environment variable and appends to create secret name that matches to secrets manager.
+    secret_name = "{}/demo-app/postgresql".format(os.environ.get('COPILOT_ENVIRONMENT_NAME')) 
     
     # Create a Secrets Manager Client
     client = boto3.client('secretsmanager')
     
-
     # Get the secret value
-
     response = client.get_secret_value(SecretId=secret_name)
     secret = json.loads(response['SecretString'])
 
@@ -62,15 +28,6 @@ def get_db_connection():
                             user=secret['username'],
                             password=secret['password']) 
     
-    """conn = psycopg2.connect(host='demo-database.cyy3xzejpvft.us-east-1.rds.amazonaws.com',
-                            database='demo_database',
-                            user='postgres',
-                            password='G8-pzvc%pe0mXAaZN!aP}5(s~dUZ')"""
-
-    """conn = psycopg2.connect(host='demo-database.cyy3xzejpvft.us-east-1.rds.amazonaws.com',
-                            database='demo_database',
-                            user=secret['username'],
-                            password=secret['password'])"""
     return conn
 
 
